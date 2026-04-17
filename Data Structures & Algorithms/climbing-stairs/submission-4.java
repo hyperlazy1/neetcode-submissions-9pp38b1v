ab316f269df70103d61594b97e41f12114e74c98
hyperlazy1 <125260121+hyperlazy1@users.noreply.github.com>
2026-04-17 17:46:11 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.java
@@ -0,0 +1,13 @@
+public class Solution {
+    int[] cache;
+    public int climbStairs(int n) {
+        cache = new int[n+1];
+        cache[0] = 1;
+        cache[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            cache[i] = cache[i-1] + cache[i-2];
+        }
+        return cache[n];
+    }
+}
