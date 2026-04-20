579e1c34b3bbdb03b4a6d7ef008bc05dc693ccf8
hyperlazy1 <125260121+hyperlazy1@users.noreply.github.com>
2026-04-20 22:03:26 +0530

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        dp[n] = 0;
+        dp[n-1] = cost[n-1];
+
+        for (int i = n-2; i >= 0; i--) {
+            dp[i] = Math.min(cost[i] + dp[i + 1] , cost[i] + dp[i + 2]);
+        }
+
+        return Math.min(dp[0],dp[1]);
+    }
+}
